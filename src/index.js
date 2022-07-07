d610c9ace1f55ca37f46eb3f3f6c3d5edc642802
Ashay Jaiswal <ashaywin@gmail.com>
2022-07-07 20:26:15 +0530

    create header

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import {loadPage} from "./modules/loadPage";
+import "./res/style.css"
+
+loadPage();
---
+++ src/modules/loadPage.js
@@ -0,0 +1,26 @@
+function generateMenuLines(){
+    const menuIcon = document.createElement("div");
+    menuIcon.classList = "menu-icon";
+
+    for(let i=0; i < 3; i++){
+        let line = document.createElement("span");
+        line.classList = "line";
+        menuIcon.appendChild(line);
+    }
+    return menuIcon;
+}
+
+export const loadPage = () => {
+    const body = document.body;
+    const header = document.createElement("header");
+
+    const menuIcon = generateMenuLines();
+    header.appendChild(menuIcon);
+
+    const logoText = document.createElement("div");
+    logoText.id = "logo";
+    logoText.textContent = "MyToDo";
+    header.appendChild(logoText);
+
+    body.appendChild(header);
+}
