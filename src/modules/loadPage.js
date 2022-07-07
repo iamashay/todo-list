5f4b89a247dbc54589f980c7a5deaa2f0c1d6d09
Ashay Jaiswal <ashaywin@gmail.com>
2022-07-07 22:44:15 +0530

    set base for the content area

--- src/modules/loadPage.js
+++ src/modules/loadPage.js
@@ -1,3 +1,17 @@
+function generateHeader(){
+    const header = document.createElement("header");
+
+    const menuIcon = generateMenuLines();
+    header.appendChild(menuIcon);
+
+    const logoText = document.createElement("div");
+    logoText.id = "logo";
+    logoText.textContent = "MyToDo";
+    header.appendChild(logoText);
+
+    return header;
+}
+
 function generateMenuLines(){
     const menuIcon = document.createElement("div");
     menuIcon.classList = "menu-icon";
@@ -10,17 +24,34 @@ function generateMenuLines(){
     return menuIcon;
 }
 
+function generateSidebar() {
+    const sidebar = document.createElement("div");
+    sidebar.id = "sidebar";
+    sidebar.textContent = "SIDEBAR"
+    return sidebar;
+}
+
+function generateTaskContainer(){
+    const taskContainer = document.createElement("div");
+    taskContainer.id = "task-container";
+    taskContainer.textContent = "TASKS' CONTAINER"
+    return taskContainer;
+}
+
 export const loadPage = () => {
     const body = document.body;
-    const header = document.createElement("header");
+    const header = generateHeader();
 
-    const menuIcon = generateMenuLines();
-    header.appendChild(menuIcon);
+    const mainContainer = document.createElement("div");
+    mainContainer.id = "main-container";
 
-    const logoText = document.createElement("div");
-    logoText.id = "logo";
-    logoText.textContent = "MyToDo";
-    header.appendChild(logoText);
+    const sidebar = generateSidebar();
+    const taskContainer = generateTaskContainer();
+
+    mainContainer.appendChild(sidebar);
+    mainContainer.appendChild(taskContainer);
 
     body.appendChild(header);
+    body.appendChild(mainContainer);
+
 }
