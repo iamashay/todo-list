c6cd09456a1c0061552c6cdcdd8fda65f762cbbf
Ashay Jaiswal <ashaywin@gmail.com>
2022-07-10 20:41:04 +0530

    add options to sidebar

--- src/modules/loadPage.js
+++ src/modules/loadPage.js
@@ -24,10 +24,65 @@ function generateMenuLines(){
     return menuIcon;
 }
 
+function generateSidebarOption(title) { //currently creating a seperate function for the sidebar options
+    const optionId = title.toLowerCase().replaceAll(" ", "-");
+    
+    const option = document.createElement("div");
+    option.classList = "sidebar-option";
+    option.id = optionId;
+    const optionImg = new Image();
+    optionImg.src = require(`../res/imgs/${optionId}.png`);
+    optionImg.alt = title+" icon";
+    
+    const optionTitle = document.createElement("span");
+    optionTitle.class = "option-title";
+    optionTitle.textContent = title;
+
+    option.appendChild(optionImg);
+    option.appendChild(optionTitle);
+    
+    return option;
+}
+
+function generateProjectContainer(){
+    const projectContainer = document.createElement("div");
+    const title = document.createElement("div");
+    title.textContent = "Projects"
+    title.id = "projects-heading";
+
+    const projectList = document.createElement("div");
+    projectList.id = "project-list";
+
+    const addProject = document.createElement("div");
+    addProject.classList = "add-project";
+    const addProjectImg = new Image();
+    addProjectImg.src = require("../res/imgs/add-project.png")
+    addProjectImg.alt = "add project icon"
+    const addProjectTitle = document.createElement("span");
+    addProjectTitle.textContent = "Add Project";
+
+    addProject.appendChild(addProjectImg);
+    addProject.appendChild(addProjectTitle);
+
+    projectList.appendChild(addProject);
+
+    projectContainer.appendChild(title);
+    projectContainer.appendChild(projectList);
+
+    return projectContainer;
+}
+
 function generateSidebar() {
     const sidebar = document.createElement("div");
     sidebar.id = "sidebar";
-    sidebar.textContent = "SIDEBAR"
+
+    const showAllOption = generateSidebarOption("Show All");
+    sidebar.appendChild(showAllOption)
+
+    const projectContainer = generateProjectContainer();
+    sidebar.appendChild(projectContainer)
+
+
     return sidebar;
 }
 
