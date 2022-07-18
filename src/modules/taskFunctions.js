b8d627b343e2d474d211c15a4b56cf09655a9f6f
Ashay Jaiswal <ashaywin@gmail.com>
2022-07-18 19:17:46 +0530

    Create a module for all the task related functions

---
+++ src/modules/taskFunctions.js
@@ -0,0 +1,7 @@
+import {Task} from "./taskModel"
+import {projectList} from "./projectModel"
+
+function createTask(projectName, title, dueDate, description, isImportant){
+    const newTask = new Task(title, dueDate, description, isImportant);
+    
+}
