da12d3ddea81280d59faab2398d58fe7675bd748
Ashay Jaiswal <ashaywin@gmail.com>
2022-07-21 22:53:39 +0530

    create a function for editing task

--- src/modules/taskFunctions.js
+++ src/modules/taskFunctions.js
@@ -3,4 +3,9 @@ import {projectList} from "./projectModel"
 
 function createTask(projectName, title, dueDate, description, isImportant){
     const newTask = new Task(title, dueDate, description, isImportant);
+    projectList[projectName].taskList.push(newTask);
+}
+
+function editTask(projectName, value){
+    
 }
