b1bfe8a5f97f1bc9e7690851771b7d6708371e7d
Ashay Jaiswal <ashaywin@gmail.com>
2022-07-24 01:13:08 +0530

    Add edit, delete functions for tasks

--- src/modules/taskFunctions.js
+++ src/modules/taskFunctions.js
@@ -4,8 +4,20 @@ import {projectList} from "./projectModel"
 function createTask(projectName, title, dueDate, description, isImportant){
     const newTask = new Task(title, dueDate, description, isImportant);
     projectList[projectName].taskList.push(newTask);
+    return newTask;
 }
 
-function editTask(projectName, value){
-    
+function editTask(projectName, taskID, jsonValue){
+    const myTask = projectList[projectName][taskID];
+    myTask.title = jsonValue.title;
+    myTask.dueDate = jsonValue.dueDate;
+    myTask.description = jsonValue.description;
+    myTask.isImportant = jsonValue.isImportant;
+    return myTask;
 }
+
+function deleteTask(projectName, taskID){
+    const myProject = projectList[projectName];
+    myProject.splice(taskID, 1);
+}
+
