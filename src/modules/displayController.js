40a410ab6f6534c8204ca9f1c170a7fff62b7469
Ashay Jaiswal <ashaywin@gmail.com>
2022-07-24 16:59:30 +0530

    Create a displayController for DOM manipulation

---
+++ src/modules/displayController.js
@@ -0,0 +1,11 @@
+import * as projectFunction from "./projectFunctions.js"
+import * as taskFunction from "./taskFunctions.js"
+import { compareAsc, format } from "date-fns"
+import { qs } from "./globalFunctions.js"
+
+const taskListContainer = qs(".task-list-container");
+const projectListContainer = qs("#project-list");
+
+function displayController() {
+
+}
